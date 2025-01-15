b438b852a285db61617bc6940427c5f456bb9558
Yoann Robert <luneroka@gmail.com>
2025-01-15 10:57:22 +0100

    Created update_contact route

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,13 @@ from flask import request, jsonify
 from config import app, db
 from models import Contact
 
+
 @app.route("/contacts", methods=["GET"])
 def get_contacts():
   contacts = Contact.query.all()
   json_contacts = list(map(lambda x: x.to_json(), contacts))
   return jsonify({"contacts": json_contacts})
+
 
 @app.route("/create_contact", methods=["POST"])
 def create_contact():
@@ -27,6 +29,24 @@ def create_contact():
     return jsonify({"message": str(e)}), 400
 
   return jsonify({"message": "User created!"}), 201
+
+
+@app.route("/update_contact/<int:user_id>", methods=["PATCH"]):
+def update_contact(user_id):
+  contact = Contact.query.get(user_id)
+
+  if not contact:
+    return jsonify({"message": "User not found."}), 404
+  
+  data = request.json
+  contact.first_name = data.get("firstName", contact.first_name)
+  contact.last_name = data.get("lastName", contact.last_name)
+  contact.email = data.get("email", contact.email)
+
+  db.session.commit()
+
+  return jsonify({"message": "User updated."}), 200
+
 
 if __name__ == "__main__":
   with app.app_context():
